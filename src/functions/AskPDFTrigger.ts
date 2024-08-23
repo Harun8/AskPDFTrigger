5af39641ecd41205309b61b4db110e8b1613c0b4
Harun <harunabdi8@gmail.com>
2024-08-23 17:57:41 +0200

    changed puppeteer lib, + some local testing

--- src/functions/AskPDFTrigger.ts
+++ src/functions/AskPDFTrigger.ts
@@ -1,28 +1,41 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 import { callScraper } from "../../cypress/e2e/spec.cy";
-import puppeteer from "puppeteer-core";
+// import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
+
+import path = require("path");
 export async function AskPDFTrigger(
   myTimer: Timer,
   context: InvocationContext
 ): Promise<void> {
   context.log("Timer function processed request.");
-  scrape();
+  console.log("calling scrape!!!!!!!!!!!");
+  try {
+    await scrape(context);
+  } catch (error) {
+    context.log("Error during scraping: ", error);
+  }
 }
 
 app.timer("AskPDFTrigger", {
   schedule: "45 30 15 * * 1",
   handler: AskPDFTrigger,
 });
-async function scrape() {
+async function scrape(context: InvocationContext) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto("https://www.askpdfs.io/preview");
 
-  // You can add your scraping logic here
-  await page.waitForSelector('[id="message"]');
-  await page.type('[id="message"]', "hi");
-  await page.click("button.bg-blue-500.text-white.rounded.p-2.mt-2");
+  context.log("Scraping in progress...");
 
-  // Add other logic as needed
-  await browser.close();
+  try {
+    await page.waitForSelector('[id="message"]');
+    await page.type('[id="message"]', "hi");
+    await page.click("button.bg-blue-500.text-white.rounded.p-2.mt-2");
+    context.log("Scraping completed.");
+  } catch (err) {
+    context.log("Error during scraping: ", err);
+  } finally {
+    await browser.close();
+  }
 }
